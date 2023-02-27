a083f698509236f829b4f08674586e5c96702fc3
addicted2crypto <addicted2crypto.com>
2023-02-27 10:15:09 -0800

    daily code challenge for 02/27/2023

---
+++ codewars02272023.js
@@ -0,0 +1,35 @@
+// Description: Start a party.
+ 
+//  Your Goal: Start a Party!
+// This challenge is all about creating a group with your friends to split a shared expense.
+
+//  Setup
+// The first thing you'll need to do is create a constructor function on the Party contract which will set the deposit amount it will cost someone to RSVP to the party. This constructor function will take in a uint256 amount. Store this for later. It must be paid by the participants, or they cannot join the party!
+//  RSVP
+// Create a external, payable function called rsvp that will allow your friends to join the party. This function should keep track of the addresses joining the party so we can split the expenses in the next stage. These will be the addresses calling the function.
+//  Security
+// Ensure that anyone who RSVPs to the party sends the exactly the required deposit. If they do not send exactly this amount, revert the transaction.
+// Ensure that an address that has RSVP'd already cannot RSVP again! If the same address tries to RSVP twice, revert the transaction.
+
+// My Solution =>
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Party {
+	uint deposit;
+    address[] members;
+    mapping(address => bool) paid;
+
+    constructor(uint _deposit) {
+        deposit = _deposit;
+    }
+    function rsvp() external payable {
+        require(!paid[msg.sender]);
+        require(msg.value == deposit);
+        members.push(msg.sender);
+        paid[msg.sender] = true;
+    }
+
+
+}
